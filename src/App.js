d6aa11fa863dde803bf65976cc3b75704545e453
Andrew Li <lte1160106854@gmail.com>
2024-06-05 22:32:01 -0700

    Moved the utility functions from App.js to Buttons.js; Also added some JSDoc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import {
     useState,
-    useRef
+    useRef,
+    createContext
 } from "react";
 import "./App.css";
 import "./components/styles/Buttons.css";
-import Button from "./components/Buttons";
+import Buttons from "./components/Buttons";
 
 //  ！！！！！！！！！！！！！！！！！
 // hover on button
@@ -27,6 +28,7 @@ import Button from "./components/Buttons";
 // if value 大于99个digits， error
 // if result<=99999999, 不管有几个小数位，显示最多9 digits（if前8，后round to一个小数位)
 
+export const calculationContext = createContext();
 
 function App() {
     const inputRef = useRef(null);
@@ -40,181 +42,6 @@ function App() {
         Multiply: "Multiply"
     }
 
-    function updateInput(e) {
-        e.preventDefault();
-
-        let newValue;
-        // get the pressed number
-        let keyPressed = e.currentTarget.getAttribute("data-value");
-
-        if (input === "0") {
-            // discard the default "0"
-            newValue = keyPressed;
-        }
-        else if (keyPressed === "." && input.includes(".")) {
-            // if users pressed multiple "." for a value, 
-            // only keeping one "." is necessary
-            // ignore the extra "."
-            newValue = input;
-        }
-        else {
-            newValue = input + keyPressed;
-        }
-
-        //update the values
-        setInput(newValue);
-        if (curOperator == null) {
-            setResult(newValue);
-        }
-    }
-
-    function plus(e) {
-        e.preventDefault();
-
-        setCurOperator(Operators.Plus);
-        setInput("0");
-
-        highlightOperator(Operators.Plus);
-    };
-
-    function minus(e) {
-        e.preventDefault();
-
-        setCurOperator(Operators.Minus);
-        setInput("0");
-
-        highlightOperator(Operators.Minus);
-    };
-
-    function multiply(e) {
-        e.preventDefault();
-
-        setCurOperator(Operators.Multiply);
-        setInput("0");
-
-        highlightOperator(Operators.Multiply);
-    };
-
-    function divide(e) {
-        e.preventDefault();
-
-        setCurOperator(Operators.Divide);
-        setInput("0");
-
-        highlightOperator(Operators.Divide);
-    };
-
-    function equal(e) {
-        e.preventDefault();
-
-        const inputValue = Number(input);
-
-        if (curOperator == Operators.Plus) {
-            setResult(Number(result) + inputValue);
-        }
-        else if (curOperator == Operators.Minus) {
-            setResult(Number(result) - inputValue);
-        }
-        else if (curOperator == Operators.Multiply) {
-            setResult(Number(result) * inputValue);
-        }
-        else if (curOperator == Operators.Divide) {
-            if (inputValue == 0) {
-                alert("Cannot divide by 0!")
-                return;
-            }
-            else {
-                setResult(Number(result) / inputValue);
-            }
-        }
-
-        // reset
-        setInput("0");
-        setCurOperator(null);
-        highlightOperator(null);
-    };
-
-    function flip(e) {
-        e.preventDefault();
-
-        let newValue;
-
-        if (input === "0") {
-            // Do nothing
-            return;
-        }
-        else if (input.charAt(0) === "-") {
-            newValue = input.substring(1, input.length);
-        }
-        else {
-            newValue = "-" + input;
-        }
-
-        setInput(newValue);
-        if (curOperator == null) {
-            setResult(newValue);
-        }
-    };
-
-    function reset(e) {
-        e.preventDefault();
-
-        setResult(0);
-        setInput("0");
-        setCurOperator(null);
-
-        // reset the color of operator buttons
-        highlightOperator(null);
-    };
-
-    function backSpace(e) {
-        e.preventDefault();
-
-        let newInput = input.substring(0, input.length - 1);
-        if (newInput === "" || newInput === "-") {
-            // input by default is 0
-            newInput = "0";
-        }
-        setInput(newInput);
-
-        if (curOperator == null) {
-            setResult(newInput);
-        }
-    };
-
-
-    /**
-     * Highlight the specified operator button
-     * @param {Operators} operator - The operator button to be highlighted. 
-     *                               If value if null, no operator button will be highlighted.
-     */
-    function highlightOperator(operator) {
-        // First, dim all the operator buttons
-        let plusBtn = document.getElementById("plus");
-        plusBtn.className = "btn operators";
-        let minusBtn = document.getElementById("minus");
-        minusBtn.className = "btn operators";
-        let multiplyBtn = document.getElementById("multiply");
-        multiplyBtn.className = "btn operators";
-        let divideBtn = document.getElementById("divide");
-        divideBtn.className = "btn operators";
-
-        // Then, hightlight only the specified operator button
-        if (operator == Operators.Plus) {
-            plusBtn.className = "btn clickedOperators";
-        }
-        else if (operator == Operators.Minus) {
-            minusBtn.className = "btn clickedOperators";
-        }
-        else if (operator == Operators.Multiply) {
-            multiplyBtn.className = "btn clickedOperators";
-        }
-        else if (operator === Operators.Divide) {
-            divideBtn.className = "btn clickedOperators";
-        }
-    };
-
-
     /**
      * Represent the given number in scientific notation.
      * 
@@ -223,14 +50,14 @@ function App() {
      */
     function convertNumber(number) {
         const numberValue = Number(number);
-        
+
         // The number of the digits of the whole number part -1
-        const power = number.replace("-","").split(".")[0].length - 1;
-        
+        const power = number.replace("-", "").split(".")[0].length - 1;
+
         let base = String(numberValue / Math.pow(10, power));
 
         // only keep maximum 4 decimal places
-        if (base.replace("-", "").length > 6){
+        if (base.replace("-", "").length > 6) {
             base = roundToDecimalPlaces(base, 4);
         }
 
@@ -252,40 +79,40 @@ function App() {
 
 
     return (
-        <div className="App">
-            <div>
-                <h1>Simplest Working Calculator</h1>
+        <calculationContext.Provider
+            value={{
+                input, setInput,
+                result, setResult,
+                curOperator, setCurOperator,
+                Operators
+        }}>
+
+            <div className="App">
+                <div>
+                    <h1>Simplest Working Calculator</h1>
+                </div>
+
+                <form>
+
+                    <h1> Result: {result}</h1>
+
+                    <input
+                        pattern="[0-9]"
+                        ref={inputRef}
+                        type="number"
+                        placeholder="Type a number"
+                    />
+
+                    <Buttons/>
+
+                </form>
+
+                <br></br><br></br><br></br>
+                <h1> Input: {input}</h1>
+                <h1> {convertNumber("66.9")}</h1>
             </div>
 
-            <form>
-
-                <h1> Result: {result}</h1>
-
-                <input
-                    pattern="[0-9]"
-                    ref={inputRef}
-                    type="number"
-                    placeholder="Type a number"
-                />
-
-                <Button
-                    plus={plus}
-                    minus={minus}
-                    multiply={multiply}
-                    divide={divide}
-                    equal={equal}
-                    flip={flip}
-                    resetResult={reset}
-                    updateInput={updateInput}
-                    backSpace={backSpace}
-                />
-
-            </form>
-
-            <br></br><br></br><br></br>
-            <h1> Input: {input}</h1>
-            <h1> {convertNumber("66.9")}</h1>
-        </div>
+        </calculationContext.Provider>
     );
 }
 
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,31 +1,250 @@
-
+import {
+    useContext,
+} from "react";
+import { calculationContext } from "../App"
 import "./styles/Buttons.css";
 
+function Buttons() {
+    const {
+        input, setInput,
+        result, setResult,
+        curOperator, setCurOperator,
+        Operators
+    } = useContext(calculationContext);
 
-function Button(props) {
+    /**
+     * Process the key (for the number keys and the "." key) that the user just pressed
+     * @param e 
+     */
+    function updateInput(e) {
+        e.preventDefault();
+
+        let newValue;
+        // get the pressed number
+        let keyPressed = e.currentTarget.getAttribute("data-value");
+
+        if (input === "0") {
+            // discard the default "0"
+            newValue = keyPressed;
+        }
+        else if (keyPressed === "." && input.includes(".")) {
+            // if users pressed multiple "." for a value, 
+            // only keeping one "." is necessary
+            // ignore the extra "."
+            newValue = input;
+        }
+        else {
+            newValue = input + keyPressed;
+        }
+
+        //update the values
+        setInput(newValue);
+        if (curOperator == null) {
+            setResult(newValue);
+        }
+    }
+
+    /**
+     * Prepares for the addition operation
+     * @param e 
+     */
+    function plus(e) {
+        e.preventDefault();
+
+        setCurOperator(Operators.Plus);
+        setInput("0");
+
+        highlightOperator(Operators.Plus);
+    };
+
+    /**
+     * Prepares the substraction operation
+     * @param e 
+     */
+    function minus(e) {
+        e.preventDefault();
+
+        setCurOperator(Operators.Minus);
+        setInput("0");
+
+        highlightOperator(Operators.Minus);
+    };
+
+    /**
+     * Prepares the multiplication operation
+     * @param e 
+     */
+    function multiply(e) {
+        e.preventDefault();
+
+        setCurOperator(Operators.Multiply);
+        setInput("0");
+
+        highlightOperator(Operators.Multiply);
+    };
+
+    /**
+     * Prepares the division operation
+     * @param e 
+     */
+    function divide(e) {
+        e.preventDefault();
+
+        setCurOperator(Operators.Divide);
+        setInput("0");
+
+        highlightOperator(Operators.Divide);
+    };
+
+    /**
+     * Calculate the result of the operations
+     * @param e 
+     */
+    function equal(e) {
+        e.preventDefault();
+
+        const inputValue = Number(input);
+
+        if (curOperator == Operators.Plus) {
+            setResult(Number(result) + inputValue);
+        }
+        else if (curOperator == Operators.Minus) {
+            setResult(Number(result) - inputValue);
+        }
+        else if (curOperator == Operators.Multiply) {
+            setResult(Number(result) * inputValue);
+        }
+        else if (curOperator == Operators.Divide) {
+            if (inputValue == 0) {
+                alert("Cannot divide by 0!")
+                return;
+            }
+            else {
+                setResult(Number(result) / inputValue);
+            }
+        }
+
+        // reset
+        setInput("0");
+        setCurOperator(null);
+        highlightOperator(null);
+    };
+
+    /**
+     * Flips the sign of the input value
+     * @param e 
+     */
+    function flip(e) {
+        e.preventDefault();
+
+        let newValue;
+
+        if (input === "0") {
+            // Do nothing
+            return;
+        }
+        else if (input.charAt(0) === "-") {
+            newValue = input.substring(1, input.length);
+        }
+        else {
+            newValue = "-" + input;
+        }
+
+        setInput(newValue);
+        if (curOperator == null) {
+            setResult(newValue);
+        }
+    };
+
+    /**
+     * Resets the calculator
+     * @param e 
+     */
+    function reset(e) {
+        e.preventDefault();
+
+        setResult(0);
+        setInput("0");
+        setCurOperator(null);
+
+        // reset the color of operator buttons
+        highlightOperator(null);
+    };
+
+    /**
+     * Discards the last input digit
+     * @param e 
+     */
+    function backSpace(e) {
+        e.preventDefault();
+
+        let newInput = input.substring(0, input.length - 1);
+        if (newInput === "" || newInput === "-") {
+            // input by default is 0
+            newInput = "0";
+        }
+        setInput(newInput);
+
+        if (curOperator == null) {
+            setResult(newInput);
+        }
+    };
+
+
+    /**
+     * Highlight the specified operator button
+     * @param {Operators} operator - The operator button to be highlighted. 
+     *                               If value if null, no operator button will be highlighted.
+     */
+    function highlightOperator(operator) {
+        // First, dim all the operator buttons
+        let plusBtn = document.getElementById("plus");
+        plusBtn.className = "btn operators";
+        let minusBtn = document.getElementById("minus");
+        minusBtn.className = "btn operators";
+        let multiplyBtn = document.getElementById("multiply");
+        multiplyBtn.className = "btn operators";
+        let divideBtn = document.getElementById("divide");
+        divideBtn.className = "btn operators";
+
+        // Then, hightlight only the specified operator button
+        if (operator == Operators.Plus) {
+            plusBtn.className = "btn clickedOperators";
+        }
+        else if (operator == Operators.Minus) {
+            minusBtn.className = "btn clickedOperators";
+        }
+        else if (operator == Operators.Multiply) {
+            multiplyBtn.className = "btn clickedOperators";
+        }
+        else if (operator === Operators.Divide) {
+            divideBtn.className = "btn clickedOperators";
+        }
+    };
+
     return (
         <div className="numberpad">
-            <button onClick={props.reset} className="btn specials">AC</button>
-            <button onClick={props.flip} className="btn specials">±</button>
-            <button data-value="backspace" onClick={props.backSpace} className="btn specials">⌫</button>
-            <button id="divide" onClick={props.divide} className="btn operators"> <span className="operatorPosition">÷</span> </button>
-            <button data-value="7" onClick={props.updateInput} className="btn numbers">7</button>
-            <button data-value="8" onClick={props.updateInput} className="btn numbers">8</button>
-            <button data-value="9" onClick={props.updateInput} className="btn numbers">9</button>
-            <button id="multiply" onClick={props.multiply} className="btn operators"><span className="operatorPosition">x</span></button>
-            <button data-value="4" onClick={props.updateInput} className="btn numbers">4</button>
-            <button data-value="5" onClick={props.updateInput} className="btn numbers">5</button>
-            <button data-value="6" onClick={props.updateInput} className="btn numbers">6</button>
-            <button id="minus" onClick={props.minus} className="btn operators"><span className="operatorPosition">-</span></button>
-            <button data-value="1" onClick={props.updateInput} className="btn numbers">1</button>
-            <button data-value="2" onClick={props.updateInput} className="btn numbers">2</button>
-            <button data-value="3" onClick={props.updateInput} className="btn numbers">3</button>
-            <button id="plus" onClick={props.plus} className="btn operators"><span className="operatorPosition">+</span></button>
-            <button data-value="0" onClick={props.updateInput} className="btn numbers">0</button>
-            <button data-value="." onClick={props.updateInput} className="btn numbers">.</button>
-            <button onClick={props.equal} className="btn operators"><span className="operatorPosition">=</span></button>
+            <button onClick={reset} className="btn specials">AC</button>
+            <button onClick={flip} className="btn specials">±</button>
+            <button data-value="backspace" onClick={backSpace} className="btn specials">⌫</button>
+            <button id="divide" onClick={divide} className="btn operators"> <span className="operatorPosition">÷</span> </button>
+            <button data-value="7" onClick={updateInput} className="btn numbers">7</button>
+            <button data-value="8" onClick={updateInput} className="btn numbers">8</button>
+            <button data-value="9" onClick={updateInput} className="btn numbers">9</button>
+            <button id="multiply" onClick={multiply} className="btn operators"><span className="operatorPosition">x</span></button>
+            <button data-value="4" onClick={updateInput} className="btn numbers">4</button>
+            <button data-value="5" onClick={updateInput} className="btn numbers">5</button>
+            <button data-value="6" onClick={updateInput} className="btn numbers">6</button>
+            <button id="minus" onClick={minus} className="btn operators"><span className="operatorPosition">-</span></button>
+            <button data-value="1" onClick={updateInput} className="btn numbers">1</button>
+            <button data-value="2" onClick={updateInput} className="btn numbers">2</button>
+            <button data-value="3" onClick={updateInput} className="btn numbers">3</button>
+            <button id="plus" onClick={plus} className="btn operators"><span className="operatorPosition">+</span></button>
+            <button data-value="0" onClick={updateInput} className="btn numbers">0</button>
+            <button data-value="." onClick={updateInput} className="btn numbers">.</button>
+            <button onClick={equal} className="btn operators"><span className="operatorPosition">=</span></button>
         </div>
     );
 }
 
-export default Button; 
+export default Buttons; 
